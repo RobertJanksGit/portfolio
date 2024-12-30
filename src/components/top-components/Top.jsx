2f56d9cf73099ca5a3debe9ca088006d672c8e9a
Robert Jank <robert.jank@yahoo.com>
2024-12-30 08:42:28 -0600

    wip

--- src/components/top-components/Top.jsx
+++ src/components/top-components/Top.jsx
@@ -18,13 +18,13 @@ function Top() {
       <div className="w-1/2 pr-[20%]">
         <h1 className="text-[72px] font-bold">Some Fancy Headline here</h1>
         <p className="text-[22px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          culpa qui officia deserunt mollit anim id est laborum.'
         </p>
       </div>
       <div className="w-1/2">
