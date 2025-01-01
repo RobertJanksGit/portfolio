0c3d885ebca18ba4a38ef2106effb62171d83f81
Robert Jank <robert.jank@yahoo.com>
2025-01-01 12:16:04 -0600

    changed login button to sign in

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar({ isScrolled }) {
         onClick={toggleLogin}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       >
-        Login
+        Sign In
       </button>
       {showLogin && <Login onClose={toggleLogin} />}
     </div>
