2a9ea20eb9d75f153f4c8fc73a82b12f589a938a
Robert Jank <robert.jank@yahoo.com>
2025-01-07 15:02:19 -0600

    set up firebaseConfig

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFNnchwA-JmmCBrTot3VNF2MpS9EZSJmo",
+  authDomain: "portfolio-e472d.firebaseapp.com",
+  projectId: "portfolio-e472d",
+  storageBucket: "portfolio-e472d.firebasestorage.app",
+  messagingSenderId: "31464580943",
+  appId: "1:31464580943:web:e34440d5983a3fcb2bc73a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { firebaseApp, auth, db };
